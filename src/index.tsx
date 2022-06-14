89afb967d87d3736bd0a6a1e0798db34e25e26de
Ricardo Maharaj <ricardorobinmaharaj@gmail.com>
2022-06-13 21:18:03 -0400

    rename SW

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import { createRoot } from 'react-dom/client'
-import './index.css'
 import { App } from './App'
-import { register } from './serviceWorkerRegistration'
+import './index.css'
+import { registerSW } from './serviceWorkerRegistration'
 
 createRoot(document.getElementById('root')!).render(<App />)
 
-register()
+registerSW()
